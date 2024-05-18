fb0fae42df7b390626c67b868630ba7f11f3f246
Otavio Damo <otaviodamo15@gmail.com>
2024-05-17 22:27:04 -0300

    Projeto SmartPhone concluído!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,19 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        private string Modelo;
+
+        private string IMEI;
+
+        private int Memoria;
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
@@ -19,6 +26,11 @@ namespace DesafioPOO.Models
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
+        }
+
+        public void ApresentaCelular()
+        {
+            Console.WriteLine($"O aparelho é de número {Numero}, seu modelo é {Modelo} e possui uma memoria de {Memoria} GB");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Realiza os testes com as classes Nokia e Iphone
+
+Nokia n = new Nokia("12312", "Nokia 3", "333", 32);
+Iphone p = new Iphone("93882", "Iphone 15", "111", 512);
+
+Console.WriteLine("Apresentação do Nokia!");
+n.ApresentaCelular();
+Console.WriteLine("\n");
+Console.WriteLine("Apresentação do iPhone!");
+p.ApresentaCelular();
+Console.WriteLine("\n");
+n.Ligar();
+p.Ligar();
+Console.WriteLine("\n");
+n.ReceberLigacao();
+p.ReceberLigacao();
+
+
